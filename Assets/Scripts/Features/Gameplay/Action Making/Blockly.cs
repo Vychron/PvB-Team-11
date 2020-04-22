ad1a6c9e0e4b0fbc4f62a93ed364fa4673624c68
Vychron <demi.a.joseph@gmail.com>
2020-04-22 12:10:31 +0200

    Add summaries

--- Assets/Scripts/Features/Gameplay/Action Making/Blockly.cs
+++ Assets/Scripts/Features/Gameplay/Action Making/Blockly.cs
@@ -15,11 +15,17 @@ public abstract class Blockly : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     protected static Image _hImage => HoverText.Instance?.GetComponent<Image>();
 
+    /// <summary>
+    /// Pointer event for hovering over a block.
+    /// </summary>
     public virtual void OnPointerEnter(PointerEventData eventData) {
         SetText();
         StartCoroutine(ShowText());
     }
 
+    /// <summary>
+    /// Short delay before showing the hover text.
+    /// </summary>
     public static IEnumerator ShowText() {
         yield return new WaitForSeconds(0.5f);
         _hText.transform.parent.position = Input.mousePosition;
@@ -27,20 +33,34 @@ public abstract class Blockly : MonoBehaviour, IPointerEnterHandler, IPointerExi
         _hImage.enabled = true;
     }
 
+    /// <summary>
+    /// Pointer event for when moving the mouse off of a block.
+    /// </summary>
     public virtual void OnPointerExit(PointerEventData eventData) {
         _hText.enabled = false;
         _hImage.enabled = false;
         StopAllCoroutines();
     }
 
+    /// <summary>
+    /// Gets the text that will be displayed as hover text.
+    /// </summary>
+    /// <returns>Returns a string containing the hover text to be displayed.</returns>
     public virtual string GetText() {
         return " ";
     }
 
+    /// <summary>
+    /// Sets the hover text's text.
+    /// </summary>
     public virtual void SetText() {
         _hText.text = GetText();
     }
 
+    /// <summary>
+    /// Gets how deep the block is nested in a blockly tree.
+    /// </summary>
+    /// <returns>Returns the nested depth of the block.</returns>
     public virtual int GetDepth() {
         int currentDepth = 0;
         Blockly parent = transform.parent.parent?.GetComponent<Blockly>();
