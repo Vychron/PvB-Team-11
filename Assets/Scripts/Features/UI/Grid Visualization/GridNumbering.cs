fcb71226926a9856a097a7a93f7f13c51ae9ba07
Vychron <21598@ma-web.nl>
2020-05-07 15:20:13 +0200

    Add Visualization of grid
    Add Visual grid overlay to scene
    Add Coordinates to borders of screen
    Add Coordinate scroll along screen movement
    
    Edit Improve prefab structure of world objects
    Edit Update house sprites

---
+++ Assets/Scripts/Features/UI/Grid Visualization/GridNumbering.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridNumbering : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _coordinate;
+
+    [SerializeField]
+    private bool _reversedOrder = false;
+
+    void Start() {
+        int size = LevelGrid.Instance.GetGrid.Length;
+        if (_reversedOrder)
+            _coordinate.GetComponent<Text>().text = size.ToString();
+
+        for (int i = 0; i < size; i++) {
+            GameObject coordinate = Instantiate(_coordinate, transform);
+            if (!_reversedOrder)
+                coordinate.GetComponent<Text>().text = (i + 1).ToString();
+            else
+                coordinate.GetComponent<Text>().text = (size - i - 1).ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Features/UI/Grid Visualization/GridVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GridVisualizer : MonoBehaviour {
+
+    private Vector3 _zeroPos = Vector3.zero;
+
+    [SerializeField]
+    private Transform
+        _hor = null,
+        _vert = null,
+        _backdrop = null;
+
+    private void Update() {
+        _zeroPos = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        _zeroPos.z = 0;
+        _zeroPos.x += gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        _zeroPos.y += gameObject.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        _vert.position = new Vector3(-16 + _zeroPos.x, _vert.position.y);
+        _hor.position = new Vector3(_hor.position.x, -16 + _zeroPos.y);
+    }
+}
