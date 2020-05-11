b6f5d69f15e94715340a2bfd1e4a9d0c20306db4
Vychron <demi.a.joseph@gmail.com>
2020-05-11 11:06:14 +0200

    Add summary

--- Assets/Scripts/Features/Gameplay/Action Making/Actions/Action.cs
+++ Assets/Scripts/Features/Gameplay/Action Making/Actions/Action.cs
@@ -12,6 +12,9 @@ public abstract class Action : Blockly {
         _satisfaction = 0,
         _appreciation = 0;
 
+    /// <summary>
+    /// Returns the hunger, boredom, satisfaction and appreciation values of the action as Vector4.
+    /// </summary>
     public Vector4 GetVillagerCosts {
         get { return new Vector4(_hunger, _boredom, _satisfaction, _appreciation); }
     }
