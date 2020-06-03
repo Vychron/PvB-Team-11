893966c23e0054fa82024167331b4dc89a6d84a5
Vychron <21598@ma-web.nl>
2020-06-03 11:26:37 +0200

    Add Main menu

---
+++ Assets/Scripts/Features/UI/Minor Additions/MainMenuButtons.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _closeButton = null;
+
+#if UNITY_WEBGL
+    private void Start() {
+        _closeButton?.gameObject.SetActive(false);
+    }
+#endif
+
+    /// <summary>
+    /// Starts the game.
+    /// </summary>
+    public void StartButton() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Closes the game.
+    /// </summary>
+    public void CloseButton() {
+        Application.Quit();
+    }
+
+}
